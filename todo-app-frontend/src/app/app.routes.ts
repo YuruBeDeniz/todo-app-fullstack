504fd58303fb04b3eb7564b176507bb74bffb127
YuruBeDeniz <saganak.deniz@gmail.com>
2024-10-08 13:16:47 +0200

    add navbar and routing

--- todo-app-frontend/src/app/app.routes.ts
+++ todo-app-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TodoComponent } from './components/todo/todo.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent }, 
+    { path: 'todos', component: TodoComponent }, 
+];
